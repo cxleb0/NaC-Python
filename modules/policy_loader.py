d951e7ef9a309403942120cfb9f80635b7447b26
cxleb0 <calebgarza53@proton.me>
2026-07-19 18:14:55 -0500

    refactored policy_loader try except statements

--- modules/policy_loader.py
+++ modules/policy_loader.py
@@ -10,18 +10,16 @@ POLICY_FILE = BASE_DIR/"policies"/"firewall_policies.yaml"
 def load_policies() -> dict[str,Policy]:
     try:
         with POLICY_FILE.open("r", encoding="utf-8") as file:
-            try: 
-                data = yaml.safe_load(file)
-            except yaml.YAMLError as e:
-                raise PolicyLoadFail(f"Failed to load YAML file.")
-            policies = {}
-            for name, config in data["policies"].items():
-                policies[name] = Policy(
-                    name=name,
-                    enabled=config.get("enabled", False),
-                    severity=config.get("severity","LOW"),
-                    message=config.get("message","")            
-                )
-            return policies
+            data = yaml.safe_load(file)
     except FileNotFoundError as e:
-        raise PolicyLoadFail(f"Error loading policy {POLICY_FILE}") from e 
+        raise PolicyLoadFail(f"Failed to load YAML file.") from e
+    policies = {}
+    for name, config in data["policies"].items():
+        policies[name] = Policy(
+            name=name,
+            enabled=config.get("enabled", False),
+            severity=config.get("severity","LOW"),
+            message=config.get("message","")            
+        )
+    return policies
+     
