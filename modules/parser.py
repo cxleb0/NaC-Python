b338781baadcb472679006e4e1ce8ede8a07c3ab
cxleb0 <calebgarza53@proton.me>
2026-08-20 20:26:56 -0500

    fixed variable shadowing

--- modules/parser.py
+++ modules/parser.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import xml.etree.ElementTree as xml
 
 from modules.exceptions import ConfigParseError
@@ -15,28 +14,27 @@ def get_rules(content: str) -> list[FirewallRule]:
     Raises:
          ConfigParseError: If xml is invalid or malformed.
     """
-    
+
     try:
         root = xml.fromstring(content)
     except xml.ParseError as e:
         raise ConfigParseError("Error in parsing xml configuration.") from e
     entries = root.findall(".//rulebase/security/rules/entry")
     rules: list[FirewallRule] = []
-    
-    for e in entries:
-        name = e.get("name")
+
+    for entry in entries:
+        name = entry.get("name")
         if name is None:
             raise ValueError("Missing Name Attribute")
         rule = FirewallRule(
             name=name,
-            source=[s.text for s in e.findall("./source/member") if s.text],
-            destination=[d.text for d in e.findall("./destination/member") if d.text],
-            application=[a.text for a in e.findall("./application/member") if a.text],
-            description=(e.findtext("description") or "").strip(),
-            disabled=((e.findtext("disabled") or "no").lower() == "yes"),
-            log_end=((e.findtext("log-end") or "no").lower() == "yes")
+            source=[s.text for s in entry.findall("./source/member") if s.text],
+            destination=[d.text for d in entry.findall("./destination/member") if d.text],
+            application=[a.text for a in entry.findall("./application/member") if a.text],
+            description=(entry.findtext("description") or "").strip(),
+            disabled=((entry.findtext("disabled") or "no").lower() == "yes"),
+            log_end=((entry.findtext("log-end") or "no").lower() == "yes"),
         )
-
 
         rules.append(rule)
     return rules
