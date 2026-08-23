408676e342c143f03f93d64eaa1ab50e689092a5
cxleb0 <calebgarza53@proton.me>
2026-08-23 18:38:52 -0500

    fixed docstring inconsistencies

--- modules/exceptions.py
+++ modules/exceptions.py
@@ -1,11 +1,14 @@
 class FirewallAuditError(Exception):
     """Base exception for audit tool."""
 
+
 class ConfigLoadError(FirewallAuditError):
-    "Raise error when config file cannot be loaded."
+    """Raise error when config file cannot be loaded."""
+
 
 class ConfigParseError(FirewallAuditError):
     """Raise error when XML parcing fails."""
+
 
 class PolicyLoadError(FirewallAuditError):
     """Raise error when policy loading fails"""
